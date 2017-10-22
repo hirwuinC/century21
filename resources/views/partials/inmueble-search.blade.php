613c9728131e621e5c07049a0a2976dd25995821
Homero Hernandez <hsh283@gmail.com>
2017-10-22 12:11:56 -0300

    agregando pantalla search

---
+++ resources/views/partials/inmueble-search.blade.php
@@ -0,0 +1,29 @@
+<div class="col-sm-6">
+    <div class="thumbProperty">
+        <div class="contentTop">
+            <img src="images/{{$img}}" alt="">
+            <div class="caption">
+                <div class="businessType">
+                    <p>{{$type}}</p>
+                </div>
+                <div class="priceProject">
+                    <p><span>Bsf.:</span> {{$precio}}</p>
+                </div>
+            </div>
+        </div>
+        <div class="contentInfo">
+            <div class="infoProperty">
+                <h4>{{$titulo}}</h4>
+                <p><span><i class="fa fa-map-marker" aria-hidden="true"></i></span>{{$direccion}}</p>
+            </div>
+            <div class="characteristicsProperty">
+                <ul>
+                    <li><i class="fa fa-object-group" aria-hidden="true"></i> {{$metros}} Mts</li>
+                    <li><i class="fa fa-bed" aria-hidden="true"></i> {{$cuartos}}</li>
+                    <li><i class="fa fa-bath" aria-hidden="true"></i> {{$baños}}</li>
+                    <li><i class="fa fa-car" aria-hidden="true"></i> {{$estacionamientos}}</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
